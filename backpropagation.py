4f1809baeba15cdcdfc44c66dbaeb64a152bc254
Julia Guenther <juliag1@alumni.stanford.edu>
2017-02-11 19:26:33 -0800

    Added implementation of backpropagation

---
+++ backpropagation.py
@@ -0,0 +1,41 @@
+import numpy as np
+
+# Practicei implementing one backpropagation update step for
+# two sets of weights
+#
+# Based on an exercise from Udacity.com
+###################################################################
+
+def sigmoid(x):
+    """
+    Calculate sigmoid
+    """
+    return 1 / (1 + np.exp(-x))
+
+
+x = np.array([0.5, 0.1, -0.2])
+target = 0.6
+learnrate = 0.5
+
+weights_input_hidden = np.array([[0.5, -0.6],
+                                 [0.1, -0.2],
+                                 [0.1, 0.7]])
+
+weights_hidden_output = np.array([0.1, -0.3])
+
+hidden_layer_input = np.dot(x, weights_input_hidden)
+hidden_layer_output = sigmoid(hidden_layer_input)
+
+output_layer_in = np.dot(hidden_layer_output, weights_hidden_output)
+output = sigmoid(output_layer_in)
+
+error = target - output
+
+del_err_output = error * output * (1 - output)
+
+del_err_hidden = np.dot(del_err_output, weights_hidden_output) * \
+                 hidden_layer_output * (1 - hidden_layer_output)
+
+delta_w_h_o = hidden_layer_output * learnrate * del_err_output
+
+delta_w_i_h = learnrate * del_err_hidden * x[:,None]
